0437bc02ce95d22e8b428b8a80e19c4a68249431
Leonardo Webster <leonardowebster15@gmail.com>
2024-12-12 21:30:31 -0300

    Configurando o breeze e iniciando preparo do ambiente de criação de usuario.

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.ts',
+            ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
